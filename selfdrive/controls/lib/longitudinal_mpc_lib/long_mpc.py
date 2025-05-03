2f43049d03270217d5731c3ddc32954c0569b177
wlord1 <132536242+wlord1@users.noreply.github.com>
2025-05-03 14:18:39 -0700

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -54,17 +54,17 @@ T_IDXS = np.array(T_IDXS_LST)
 FCW_IDXS = T_IDXS < 5.0
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 COMFORT_BRAKE = 2.5
-STOP_DISTANCE = 5
+STOP_DISTANCE = 4
 CRUISE_MIN_ACCEL = -1.2
-CRUISE_MAX_ACCEL = 2.2
+CRUISE_MAX_ACCEL = 1.6
 
 def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 0.5
+    return 1.0
   elif personality==log.LongitudinalPersonality.standard:
-    return 0.5
+    return 1.0
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 0.5
+    return 1.0
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
@@ -73,9 +73,9 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
     return 1.4
   elif personality==log.LongitudinalPersonality.standard:
-    return 1.0
+    return 0.9
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 0.8
+    return 0.7
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
